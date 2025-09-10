45671df1b15d064e2cf3d132c1b103475ec555bb
Yashuchirag <chiragchandrashekar@gmail.com>
2025-09-09 19:16:13 -0700

    Added changes made to APP and ListGroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,30 @@
+import { Fragment } from "react";
+
+
 function ListGroup() {
+    let items = ["New York", "Tokyo", "London", "Sydney", "Beijing"];
+    
+    const handleClick = (event: React.MouseEvent) => {
+        console.log(event);
+    }
+
     return (
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-    )
+        <Fragment>
+            <h2>List Group</h2>
+            {items.length === 0 && <p>No items found</p>}
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li 
+                        className="list-group-item" 
+                        onClick={handleClick} 
+                        key={item}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
+    );
 }
 
 export default ListGroup;
